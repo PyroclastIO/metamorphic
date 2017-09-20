24dcc172f6fe6319b5ca3b3582dd9931cc91a45e
Michael Drogalis <mjd3089@rit.edu>
2017-09-20 15:01:38 -0700

    Fix project site.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject io.pyroclast/metamorphic "0.1.1-SNAPSHOT"
   :description "A complex event processing library for Clojure and ClojureScript."
-  :url "https://github.com/onyx-platform/metamorphic"
+  :url "https://github.com/pyroclastio/metamorphic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
